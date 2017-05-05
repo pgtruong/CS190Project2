5601337d065439175905bec6ba8e4981249f9da1
Efren Aguilar <efrenaguilar95@hotmail.com>
2017-05-05 13:16:56 -0700

    We should push just to be safe
    Yeah thats a good idea

---
+++ CS190Project2/Assets/3DModels/shotgun/RotateShotgun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateShotgun : MonoBehaviour
+{
+
+    public float rotateSpeed = 10f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.FindChild("Main Camera").transform.FindChild("Shotgun").gameObject.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
